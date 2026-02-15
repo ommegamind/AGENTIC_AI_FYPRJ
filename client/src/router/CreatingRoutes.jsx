a1edec9ca629e86e740e9a14b7522efe74c4aedb
garg <omgarg3000@gmail.com>
2026-02-15 12:27:21 +0530

    routes and pages initialized

--- client/src/router/CreatingRoutes.jsx
+++ client/src/router/CreatingRoutes.jsx
@@ -1,2 +1,28 @@
-import {createBrowserRouter} from "react-router"
+import {createBrowserRouter, RouterProvider} from "react-router"
+import{HomePage}from "../components/HomePage"
+import{ContactUsPage}from "../components/ContactUsPage"
+import{PrivacyPolicyPage}from "../components/PrivacyPolicyPage"
+import{TermsOfServicePage}from "../components/TermsOfServicePage"
 
+let router=createBrowserRouter([
+        {
+            path:"/",
+            Component:HomePage
+        },
+        {
+            path:"/contact-us",
+            Component:ContactUsPage
+        },
+        {
+            path:"/privacy-policy",
+            Component:PrivacyPolicyPage
+        },
+        {
+            path:"/terms-of-service",
+            Component:TermsOfServicePage
+        }
+    ])
+
+export const Routes=(children)=>{
+    return <RouterProvider router={router}>{children}</RouterProvider>
+}
