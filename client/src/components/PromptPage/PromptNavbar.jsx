47a4b3807965c5f32840eb8ba56b57490bdca7fd
garg <omgarg3000@gmail.com>
2026-04-19 13:10:52 +0530

    menu icon issue fixed

--- client/src/components/PromptPage/PromptNavbar.jsx
+++ client/src/components/PromptPage/PromptNavbar.jsx
@@ -30,7 +30,7 @@ import styles from "./styles/Sidebar.module.css";
 
 /* ── Inline SVG icons (no external dep) ── */
 const Icon = {
-  MenuIcon : () => (
+  Menu : () => (
   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M3 6H21M3 12H21M3 18H21"
