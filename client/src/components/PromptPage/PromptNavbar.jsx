25f6c7a7d8f3e928cca252360261ef482e938044
garg <omgarg3000@gmail.com>
2026-04-19 13:26:11 +0530

    hardcoding the color of menu and send icon

--- client/src/components/PromptPage/PromptNavbar.jsx
+++ client/src/components/PromptPage/PromptNavbar.jsx
@@ -34,7 +34,7 @@ const Icon = {
   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M3 6H21M3 12H21M3 18H21"
-      stroke="currentColor"
+      stroke="#ffffff"
       strokeWidth="2"
       strokeLinecap="round"
     />
--- client/src/components/PromptPage/PromptPage.jsx
+++ client/src/components/PromptPage/PromptPage.jsx
@@ -148,14 +148,14 @@ const SendIcon = () => (
   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M22 2L11 13"
-      stroke="currentColor"
+      stroke="#ffffff"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M22 2L15 22L11 13L2 9L22 2Z"
-      stroke="currentColor"
+      stroke="#ffffff"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
